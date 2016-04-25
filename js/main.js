97b8b59ba2e707653f081acc6a51f094d01b4a18
Andy Nguyen <nguyenandyh@gmail.com>
2016-04-24 23:37:07 -0700

    feat: Check for existing username in users table and display errors to UI

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@ var LoginForm = React.createClass({
     // TODO: Load create user component
     ReactDOM.unmountComponentAtNode(document.getElementById('content')); // Unmount loginbox
     ReactDOM.render(  // Load Create Account page
-      <CreateAcct/>,
+      <CreateAcct />,
       document.getElementById('content')
     );
   },
@@ -119,41 +119,90 @@ var LoginForm = React.createClass({
 });
 
 var CreateAcct = React.createClass({
-  // TODO: handleSubmit should also clear state, logout the current session which should go back to login page
+  // TODO: handleSubmit to create a new entry in the users table and record user info
   getInitialState: function() {
-    return {};
+    return {
+      email: '',
+      password: '',
+      password2: '',
+      username: '',
+      error: {
+        vis: false,
+        msg: ''
+      }
+    };
   },
   handleEmailChange: function(e) {
-    this.setState({ email: e.target.value });
+    this.setState({ email: e.target.value, error: { vis: false } });
   },
   handlePassChange: function(e) {
-    this.setState({ password: e.target.value });
+    this.setState({ password: e.target.value, error: { vis: false } });
   },
   handlePassChange2: function(e) {
-    tthis.setState({ password2: e.target.value });
+    this.setState({ password2: e.target.value, error: { vis: false } });
   },
   handleUserChange: function(e) {
-    this.setState({ username: e.target.value });
+    this.setState({ username: e.target.value, error: { vis: false } });
   },
   handleSubmit: function(e) {
+    var self = this;
     e.preventDefault();
     console.log("Attempting to create user account for ", this.state.email);
     var fireBaseURL = new Firebase('https://unitprice.firebaseio.com/');
-    fireBaseURL.createUser({
-      email    : this.state.email,
-      password : this.state.password
-    }, function(error, userData) {
-      if (error) {
-        console.log("Error creating user:", error);
-      } else {
-        console.log("Successfully created user account with uid:", userData.uid);
-      }
+    var usersTable = fireBaseURL.child('users');
+    /* Check if username is taken */
+    
+    if ( this.checkUsernameExists(this.state.username, usersTable) ) {
+      var message = 'Sorry, username: ' + this.state.username + 'is taken';
+      this.setState({ error: { vis: true, msg: message } });
+    /* Check if passwords Match */
+    } else if (this.state.password != this.state.password2) {
+      this.setState({ error: { vis: true, msg: 'Passwords do not match' } });
+    } else {
+      fireBaseURL.createUser({
+        email    : this.state.email,
+        password : this.state.password
+      }, function(error, userData) {
+        if (error) {
+          console.log("Error creating user:", error);
+          self.setState({ error: { vis: true, msg: error }, password: '', password2: '' });
+        } else {
+          self.createUser(); // record user info to table
+          console.log("Successfully created user account with uid:", userData.uid);
+          console.log(userData);
+          self.setState({
+            email: '',
+            password: '',
+            password2: '',
+            username: '',
+            error: {
+              vis: false,
+              msg: ''
+            }
+          }); // clear form
+          ReactDOM.unmountComponentAtNode(document.getElementById('content')); // Unmount loginbox
+          ReactDOM.render(  // Load Login page
+            <LoginBox />,
+            document.getElementById('content')
+          );
+        }
+      });
+    }
+  },
+  createUser: function() {
+      return true;
+  },
+  // TODO: refactor this to be reusable to check for anything with a child path
+  checkUsernameExists: function (username, usersTable) {
+    usersTable.orderByChild("username").equalTo(username).once("value", function(snapshot) {
+      return (snapshot.val() !== null);
     });
   },
   render: function() {
     return (
       <div class="createAcct">
         <h1>Create an Account</h1>
+        <div class="errorMsg" visible={ this.state.error.vis }>{ this.state.error.msg }</div>
         <form class="createAcctForm" onSubmit={this.handleSubmit} >
           <p>
             <input
